08df4e72c3aca7c869071fa60748cb45c9d6410a
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-03-24 11:37:24 -0400

    Simualted my first two layer neural network

--- Neuralnetworktesting/Learningneural.cs
+++ Neuralnetworktesting/Learningneural.cs
@@ -3,6 +3,48 @@
 
 class Learningneural
 {
+    //z = (x1 * w1) + (x2 * w2) + b
+    //o(z) = 1/ 1 + (e^-z)
+    // prediction = x * w
+    //loss = Math.Pow(prediction - y, 2)
+    //error = prediction - target
+
+    //two layer neural network
+    public static void Main()
+    {
+        double x = 3;//input value
+
+        double w1 = 2;     // weight to hidden neuron 1
+        double w2 = 2;     // weight to hidden neuron 2
+
+        double learningrate = 0.1;
+
+        double h1 = x * w1;
+        double h2 = x * w2;
+
+
+        double w3 = 0.5;  // hidden1 → output
+        double w4 = -0.4; // hidden2 → output
+        double y = 1;//y val
+        double output = (h1 * w3) + (h2 * w4);//output for neruons
+
+        double w3gradient = 2 * h1 * (output - y);
+
+        double neww3 = w3 - (learningrate * w3gradient); //updated weight for neuron 1 or w3
+
+        double w4gradient = 2 * h2 * (output - y); //w4 gradient 
+
+        double neww4 = w4 - (learningrate * w4gradient);//new w4 weight
+
+
+        Console.WriteLine("Output: " + output);
+        Console.WriteLine("w3 Gradient: " + w3gradient);
+        Console.WriteLine("New w3: " + neww3);
+        Console.WriteLine("w4 Gradient: " + w4gradient);
+        Console.WriteLine("New w4: " + neww4);
+
+    }
+
 
 
     /*
@@ -62,22 +104,5 @@ class Learningneural
     */
 
 
-    public static void Main()
-    {
-        double x = 2;
-
-        double w1 = 1.0;     // weight to hidden neuron 1
-        double w2 = -1.0;     // weight to hidden neuron 2
-
-        double h1 = x * w1;
-        double h2 = x * w2;
-
-
-        double w3 = 0.5;  // hidden1 → output
-        double w4 = 0.5; // hidden2 → output
-
-        double output = (h1 * w3) + (h2 * w4);
-        Console.WriteLine(output);
-    }
 }
 
