8a044ce273184561440b8b5da7c6be89c954ef1b
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-03-24 07:55:42 -0400

    added more tests

--- Neuralnetworktesting/Learningneural.cs
+++ Neuralnetworktesting/Learningneural.cs
@@ -27,24 +27,57 @@ class Learningneural
 
 
 
+    /*
     public static void Main()
     {
-        double x = 5;
-        double w = -2.4;
-        double y = 0;
-        double n = 0.1;
+        double x = 3;
+        double y = 1;
+        double w = 0.05;
+        double learningRate = 0.05;
+     for(int i = 0; i < 10; i++)
+        {
+            double prediction = x * w;
+            Console.WriteLine("Prediction: " + prediction);
+            w = Train(x, y, w, learningRate);
+            Console.WriteLine("Step" + i +":" + w);
+            double loss = Math.Pow(prediction - y, 2);
+            Console.WriteLine("Loss: " + loss);
 
+        }
+    }
+
+    public static double Train(double x, double y, double w, double learningRate)
+    {
 
         double prediction = x * w;
 
-        double gradient = 2 * x *(prediction - y);
+        double gradient = 2 *x *(prediction - y);
 
-        double newWeight = w - (n * gradient);
+        double newweight = w - (learningRate * gradient);
 
-        Console.WriteLine(prediction);
-        Console.WriteLine(gradient);
-        Console.WriteLine(newWeight);
+        return newweight;
+
+
     }
+    */
 
+
+    public static void Main()
+    {
+        double x = 2;
+
+        double w1 = 1.0;     // weight to hidden neuron 1
+        double w2 = -1.0;     // weight to hidden neuron 2
+
+        double h1 = x * w1;
+        double h2 = x * w2;
+
+
+        double w3 = 0.5;  // hidden1 → output
+        double w4 = 0.5; // hidden2 → output
+
+        double output = (h1 * w3) + (h2 * w4);
+        Console.WriteLine(output);
+    }
 }
 
